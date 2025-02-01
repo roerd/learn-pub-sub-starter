43a6f59a5276826c6314adc5d53b89681d5a5356
Rörd Hinrichsen <roerdhh@gmail.com>
2025-02-01 02:44:52 +0100

    Add amqp support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const url = "amqp://guest:guest@localhost:5672/"
+	c, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+	fmt.Println("Connection was successful")
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	fmt.Println("Shutting down...")
+	c.Close()
 }
