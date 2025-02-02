bb3aa718638ad7b9544fc4a69e93896f002a1355
Rörd Hinrichsen <roerdhh@gmail.com>
2025-02-02 17:57:54 +0100

    Make client connect to rabbitmq and create pause queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	const url = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection was successful")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.Transient)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	fmt.Println("Shutting down...")
+	conn.Close()
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -16,3 +16,32 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		Body:        val_json,
 	})
 }
+
+type SimpleQueueType int
+
+const (
+	Durable SimpleQueueType = iota
+	Transient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return ch, amqp.Queue{}, err
+	}
+	q, err := ch.QueueDeclare(queueName, simpleQueueType == Durable, simpleQueueType == Transient, simpleQueueType == Transient, false, nil)
+	if err != nil {
+		return ch, q, err
+	}
+	err = ch.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return ch, q, err
+	}
+	return ch, q, err
+}
