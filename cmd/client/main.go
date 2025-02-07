02d38033e7d787df61631b15f94310efe667c379
Rörd Hinrichsen <roerdhh@gmail.com>
2025-02-07 02:52:17 +0100

    Add game loop to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -30,9 +27,35 @@ func main() {
 	}
 	pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.Transient)
 
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	<-sigs
+	gamestate := gamelogic.NewGameState(username)
+game_loop:
+	for {
+		words := gamelogic.GetInput()
+		switch words[0] {
+		case "spawn":
+			err := gamestate.CommandSpawn(words)
+			if err != nil {
+				fmt.Println(err)
+			}
+		case "move":
+			_, err := gamestate.CommandMove(words)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("Moving worked")
+			}
+		case "status":
+			gamestate.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			break game_loop
+		default:
+			fmt.Println("Command not understood")
+		}
+	}
 	fmt.Println("Shutting down...")
-	conn.Close()
 }
